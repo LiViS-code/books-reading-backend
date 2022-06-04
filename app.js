63eeb6f439425cc17e5893754b35e82dd0ee46c7
VadymDenysiuk <80970410371q@gmail.com>
2022-06-04 19:06:10 +0300

    add registration endpoint

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const swaggerDocument = require('./swagger.json');
 require("dotenv").config();
 
 const app = express();
+const authRouter = require('./routes/api/auth')
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/auth', authRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
