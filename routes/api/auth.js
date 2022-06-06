e12b24a966157e58aee42c8e95bc414497c5c55f
VadymDenysiuk <80970410371q@gmail.com>
2022-06-06 20:43:17 +0300

    add files and methods for google auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, logout } = require("../../controllers");
+const { register, login, logout, googleAuth, googleRedirect } = require("../../controllers");
 const { auth } = require("../../middlewares");
 
 const router = express.Router();
@@ -7,6 +7,10 @@ const router = express.Router();
 router.post("/signup", register);
 
 router.post("/signin", login);
+
+router.get("/google", googleAuth);
+
+router.get("/google-redirect", googleRedirect);
 
 router.get("/logout", auth, logout);
 
