c24ac6beba38d5aa4f1f89f00532e31aa567977d
VadymDenysiuk <80970410371q@gmail.com>
2022-06-12 14:41:38 +0300

    fixed method

--- controllers/trainings/getTrainings.js
+++ controllers/trainings/getTrainings.js
@@ -3,9 +3,15 @@ const { Training } = require('../../models/training');
 const getTrainings = async (req, res) => {
   const userId = req.user._id;
 
-  const trainings = await Training.find({ userId });
+  const training = await Training.find({ owner: userId });
+
+  if(!training) {
+    return res.status(400).json({
+      message: "The user has no training yet ",
+    });
+  }
   
-  res.status(200).json({ trainings });
+  res.status(200).json({ training });
 };
 
 module.exports = getTrainings;
