74fddb66c82c59910b97ed1a5d5dedb089869a20
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-06-15 08:42:39 +0300

    fix index

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,7 @@
 const sendMail = require("./sendMail");
 const createError = require("./createError");
-const schemaSelect = require("./schemaSelect");
 
 module.exports = {
   sendMail,
   createError,
-  schemaSelect,
 };
